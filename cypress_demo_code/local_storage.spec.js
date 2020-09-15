b2601e5c1f7d349bbaad86000b5a2177cf48a880
Srivastava Harshit <harshitsrivastava@QAIT.COM>
2020-09-15 18:24:36 +0530

    checking by deleting

--- cypress_demo_code/local_storage.spec.js
+++ cypress_demo_code/local_storage.spec.js
@@ -8,14 +8,7 @@ context('Local Storage', () => {
   // in between tests to maintain a clean state
   // sometimes we need to clear the local storage manually
 
-  it('cy.clearLocalStorage() - clear all data in local storage', () => {
-    // https://on.cypress.io/clearlocalstorage
-    cy.get('.ls-btn').click().should(() => {
-      expect(localStorage.getItem('prop1')).to.eq('red')
-      expect(localStorage.getItem('prop2')).to.eq('blue')
-      expect(localStorage.getItem('prop3')).to.eq('magenta')
-    })
-
+  
     // clearLocalStorage() yields the localStorage object
     cy.clearLocalStorage().should((ls) => {
       expect(ls.getItem('prop1')).to.be.null
